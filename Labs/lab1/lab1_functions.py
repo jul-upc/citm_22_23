430c0ac8979e725d7d9ebdc96ba26ae705052b68
Julen <julian.cayero@eurecat.org>
2022-10-11 16:14:46 +0200

    Lab 1: full

--- Labs/lab1/lab1_functions.py
+++ Labs/lab1/lab1_functions.py
@@ -1,25 +1,36 @@
 import numpy as np
+from typing import Tuple
 
-def naive_forward_elimination(A: np.ndarray, b: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-  """
-  Forward elimination function wich NOT uses partial pivoting
-  
-  """
-  return np.zeros((1,1))
+def naive_forward_elimination(A: np.ndarray, b: np.ndarray) -> Tuple[np.ndarray, np.ndarray, bool]:
+    """
+    Forward elimination function wich NOT uses partial pivoting
+    This function should implement partial pivoting
 
-def forward_elimination(A: np.ndarray, b: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-  """
-  Optimized forward elimination function wich uses partial pivoting
-  forward_elimination(A, b)
-  """
-  return (np.zeros((1,1)), np.zeros((1,1))
+    """
+
+
+    # TODO: your code here!
+    return (np.zeros((1,1)), np.zeros((1,1)), False)
+
+def forward_elimination(A: np.ndarray, b: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Optimized forward elimination function wich uses partial pivoting
+    forward_elimination(A, b)
+    """
+
+
+    # TODO: your code here!
+    return (np.zeros((1,1)), np.zeros((1,1)))
+
 
 def backtracking(At: np.ndarray, bt: np.ndarray) -> np.ndarray:
-  """
-  Backtracking(At, bt) takes a triangular and regular matrix At, and an 
-  independent term bt as input and produces a vector with the solution of such 
-  system.
-  """
+    """
+    Backtracking(At, bt) takes a triangular and regular matrix At, and an 
+    independent term bt as input and produces a vector with the solution of such 
+    system.
+    """
 
-  # TODO: your code here!
-  return np.zeros((1,1))
+
+    # TODO: your code here!
+    return np.zeros((1,1))
+
