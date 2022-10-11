21ea670399dda6afa1ddeabef0b743e84c7bd030
Julen <julian.cayero@eurecat.org>
2022-10-11 13:55:50 +0200

    Lab 1: first commit

---
+++ Labs/lab1/lab1_functions.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def naive_forward_elimination(A: np.ndarray, b: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+  """
+  Forward elimination function wich NOT uses partial pivoting
+  
+  """
+  return np.zeros((1,1))
+
+def forward_elimination(A: np.ndarray, b: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+  """
+  Optimized forward elimination function wich uses partial pivoting
+  forward_elimination(A, b)
+  """
+  return (np.zeros((1,1)), np.zeros((1,1))
+
+def backtracking(At: np.ndarray, bt: np.ndarray) -> np.ndarray:
+  """
+  Backtracking(At, bt) takes a triangular and regular matrix At, and an 
+  independent term bt as input and produces a vector with the solution of such 
+  system.
+  """
+
+  # TODO: your code here!
+  return np.zeros((1,1))
